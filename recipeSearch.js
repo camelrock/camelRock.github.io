654f65d630f5b14a1e5484e9824d985ceb44605e
llamaCell <cjkalani@gmail.com>
2023-10-14 17:14:18 +0100

    fixing major issues with recipefile

--- recipeSearch.js
+++ recipeSearch.js
@@ -4,10 +4,11 @@ let ingredientsList = [
 ]
 
 class Recipe {
-    constructor(id, year, ingredients) {
+    constructor(id, year, ingredients, description) {
         this.id = id;
         this.year = year;
         this.ingredients = ingredients;
+        this.description = description;
     }
     display() {
         document.getElementById("showThisRecipe").showThis = this.id;
@@ -15,10 +16,30 @@ class Recipe {
     }
 }
 let recipesList = [
-    new Recipe(1, 1989, ["bread"]),
-    new Recipe(2, 1999, ["water", "bread"])
+    new Recipe(1, 1989, ["bread"], "plain bread"),
+    new Recipe(2, 1999, ["water", "bread"], "wet bread")
 ]
-
+function findRecipes() {
+    const term = document.getElementById("ingredientsInput").value.toLowerCase();
+    let k = 0;
+    let options = [];
+    for (i in recipesList) {
+        for (j in recipesList[i].ingredients) {
+            if (recipesList[i].ingredients[j] == term) {
+                options[k] = recipesList[i];
+                k++;
+            }
+        }
+    }
+    while (k > 0) {
+        alert(`consider recipe ${options[k - 1].id} : ${options[k - 1].description} `)
+        k--;
+        if (k == 0) {
+            return;
+        }
+    }
+    document.getElementById("tellIt").style.display = "block";
+}
 
 function searchTerms() {
     let i = 0;
